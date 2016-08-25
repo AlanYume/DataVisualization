9b395f4ad77aad2945789adbe95bf940b105fc3a
AlanYume <875785679@qq.com>
2016-08-26 01:00:07 +0800

    draw world population

---
+++ country_codes.py
@@ -0,0 +1,16 @@
+from pygal.maps.world import COUNTRIES
+
+
+def get_country_code(country_name):
+    """根据指定的国家,返回 Pygal 使用的两个字母的国别码"""
+    for code, name in COUNTRIES.items():
+        # print(code + ": " + name)
+        if name == country_name:
+            return code
+    # 如果没有找到指定的国家,返回None
+    return None
+
+
+print(get_country_code('Botswana'))
+print(get_country_code('United Arab Emirates'))
+print(get_country_code('Afghanistan'))
